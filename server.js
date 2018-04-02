e2dd8fcaa5aaff894e39e1ca7e26d64e3ec2d7e8
Denys Sidorov <000scorpions000@gmail.com>
2018-04-02 23:32:56 +0300

    add config file

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 import Koa from 'koa';
+
 const app = new Koa();
+import config from './config';
+
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(3003);
+app.listen(config.server.port, () => {
+  console.log(`Listening on port ${config.server.port}`);
+});
