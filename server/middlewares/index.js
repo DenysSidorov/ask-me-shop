b077f1cf62573c5a13266aad8b98356a90caa9eb
Denys Sidorov <000scorpions000@gmail.com>
2018-04-18 23:54:26 +0300

    add sessions

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -4,21 +4,23 @@ import testTime from './00-testTime';
 import staticFiles from './002-staticFiles';
 import logger from './03-logger';
 import bodyParser from './07-bodyParser';
-import ultipartData from './08-multipartParser';
+import multipartData from './08-multipartParser';
 
 
 import views from './001-views';
 import errors from './05-errors';
-import mongooseSession from './temp/06-mongooseSession'
+import mongooseSession from './06-mongooseSession'
 
 export default (app)=>{
   const all = compose([
     testTime,
     staticFiles,
-    logger,
-    bodyParser,
-    ultipartData,
     views,
-    errors ]);
+    logger,
+    errors,
+    mongooseSession,
+    bodyParser,
+    multipartData,
+  ]);
   app.use(all);
 }
