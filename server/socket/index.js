957f34e0436ffdf38fd1eb00b47fd51d9ee6209e
Denys Sidorov <000scorpions000@gmail.com>
2018-04-22 01:35:16 +0300

    change plans

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,16 +1,20 @@
 let socketIO = require('socket.io');
 // let socketEmitter = require('socket.io-emitter');
 
+const socketioJwt = require('socketio-jwt'); // auth via JWT for socket.io
+import config from '../config/index';
+
 function socket(server){
 
   let io = socketIO(server);
+
   io.use(function(socket, next) {
     var handshakeData = socket.request;
-    console.log(handshakeData.headers, '++++++++++');
-
+    // console.log(handshakeData.headers, '++++++++++');
     next();
   });
-  io.on('connection', function (client) {
+  io.on('connection',
+    function (client) {
     console.log(client.id + ' user connected');
     client.emit('message', 'Hello User');
     client.broadcast.emit('message', '-- Wow, new user');
