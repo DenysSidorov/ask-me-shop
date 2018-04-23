8c88dd706ab0c7e470a4b4b969504cc8451b101a
Denys Sidorov <000scorpions000@gmail.com>
2018-04-23 12:44:42 +0300

    add logger without settings

---
+++ server/bunyan/index.js
@@ -0,0 +1,7 @@
+// // hi.js
+// const bunyan = require('index');
+// const log = bunyan.createLogger({name: 'myapp'});
+//
+// log.info('hi');
+// log.warn({lang: 'fr'}, 'au revoir');
+// log.error('error');
